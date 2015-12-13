43b764dfc881028fb5031701fc12317541b39bd4
jmsam81 <jmsam81@users.noreply.github.com>
2015-12-13 15:35:35 -0800

    finished with mono/stereo toggle

--- flactomp3.py
+++ flactomp3.py
@@ -15,15 +15,17 @@ import os
 import subprocess
 import pexpect
 
-def check_channels(file):
-	data = pexpect.run('ffmpeg -i ' + file)
+def check_channels(file, hi_res_path):
+	data = pexpect.run('ffmpeg -i ' + hi_res_path + file + ".flac")
+	data = str(data)
 
-	if "stereo" in data:
-		channels = 2
-	elif "mono" in data:
+	if "mono" in data:
 		channels = 1
+	elif "stereo" in data:
+		channels = 2
 
-    return channels
+	return channels
+
 
 def make_file_list(hi_res_path):
 
@@ -38,12 +40,20 @@ def make_file_list(hi_res_path):
 	
 	return file_list
 
+
 def make_command_list(file_list, hi_res_path, lo_res_path):
+	
 	command_list = []
 	for file in file_list:
-		command_list.append("ffmpeg -i \"" + hi_res_path + str(file) + ".flac\" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -out_sample_rate 48k -b:a 320k \"" + lo_res_path + str(file) + ".mp3\"")
+		channels = check_channels(str(file), hi_res_path)
+
+		if channels == 1:
+			command_list.append("ffmpeg -i \"" + hi_res_path + str(file) + ".flac\" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -out_sample_rate 48k -b:a 160k \"" + lo_res_path + str(file) + ".mp3\"")
+		if channels == 2:
+			command_list.append("ffmpeg -i \"" + hi_res_path + str(file) + ".flac\" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -out_sample_rate 48k -b:a 320k \"" + lo_res_path + str(file) + ".mp3\"")
 		
 	return command_list
+
 
 def main(): 
 
