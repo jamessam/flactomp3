4e122cb23c83c5f037dc9f9600a07b81fe33f121
jamessam <jamessam@users.noreply.github.com>
2016-12-16 15:57:31 -0800

    add wavtomp3

--- wavtomp3.py
+++ wavtomp3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 #
-# FLACtoMP3.py
+# WAVtoMP3.py
 # Author: Jim Sam
 #
-# This script turns high resolution FLAC files into mp3s for mobile devices.
+# This script turns high resolution WAV files into mp3s for mobile devices.
 # It is a work in progress, but it still works.
 #
 # It assumes you have FFMPEG installed on your system in a way that does not
@@ -14,20 +14,18 @@
 import os
 import pexpect
 import subprocess
+import linecache
 
 def check_channels(file, hi_res_path):
-	file = str(file)
-	hi_res_path = str(hi_res_path)
+    csv_file = hi_res_path+file+'_tech.csv'
+    command = ['bwfmetaedit','--out-tech='+csv_file,hi_res_path+file+'.wav']
+    subprocess.call(command)
 
-	data = pexpect.run('ffmpeg -i \"' + hi_res_path + file + '\".flac')
-	data = str(data)
-
-	if "mono" in data:
-		channels = 1
-	elif "stereo" in data:
-		channels = 2
-
-	return channels
+    line2 = linecache.getline(csv_file,2)
+    line2 = line2.split(',')
+    channels = int(line2[4])
+    os.remove(csv_file)
+    return channels
 
 
 def make_command(file, hi_res_path, lo_res_path):
@@ -39,9 +37,9 @@ def make_command(file, hi_res_path, lo_res_path):
 	channels = check_channels(file, hi_res_path)
 
 	if channels == 1:
-		command = ['ffmpeg','-i',hi_res_path+file+'.flac','-write_id3v1','1','-id3v2_version','3','-dither_method','modified_e_weighted','-out_sample_rate','48k','-b:a','160k',lo_res_path+file+'.mp3']
+		command = ['ffmpeg','-i',hi_res_path+file+'.wav','-write_id3v1','1','-id3v2_version','3','-dither_method','modified_e_weighted','-out_sample_rate','48k','-b:a','160k',lo_res_path+file.replace('p0', 'r0')+'.mp3']
 	if channels == 2:
-		command = ['ffmpeg','-i',hi_res_path+file+'.flac','-write_id3v1','1','-id3v2_version','3','-dither_method','modified_e_weighted','-out_sample_rate','48k','-b:a','320k',lo_res_path+file+'.mp3']
+		command = ['ffmpeg','-i',hi_res_path+file+'.wav','-write_id3v1','1','-id3v2_version','3','-dither_method','modified_e_weighted','-out_sample_rate','48k','-b:a','320k',lo_res_path+file.replace('p0', 'r0')+'.mp3']
 
 	return command
 
@@ -54,8 +52,8 @@ def make_file_list(hi_res_path):
 
 	for file in dir1:
 		file_name=str(file)
-		if file_name[-5:] == ".flac":
-			file_list.append(file_name[:-5])
+		if file_name[-4:].lower() == ".wav":
+			file_list.append(file_name[:-4])
 
 	return file_list
 
