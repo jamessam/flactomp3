74ed08cd8c0304546c93be12bb36d8d0281899d4
Jim Sam <jamessam@users.noreply.github.com>
2016-12-26 02:14:31 -0800

    tabs v. spaces redux

--- flactomp3.py
+++ flactomp3.py
@@ -42,14 +42,14 @@ def make_command(file, hi_res_path, lo_res_path):
                     '-write_id3v1', '1','-id3v2_version','3',
                     '-dither_method','modified_e_weighted',
                     '-out_sample_rate','48k','-b:a','160k',
-					lo_res_path+file+'.mp3']
+                    lo_res_path+file+'.mp3']
     if channels == 2:
         command = ['ffmpeg','-i',
                     hi_res_path+file+'.flac',
                     '-write_id3v1','1','-id3v2_version','3',
                     '-dither_method','modified_e_weighted',
                     '-out_sample_rate','48k', '-b:a','320k',
-					lo_res_path+file+'.mp3']
+                    lo_res_path+file+'.mp3']
     return command
 
 def gimmespace():
