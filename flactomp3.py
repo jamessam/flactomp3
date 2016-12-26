3a4fde4bcce17179547be0789ad387bf23832c87
Jim Sam <jamessam@users.noreply.github.com>
2016-12-26 15:58:31 -0800

    refactor flactomp3.py to be used for different file extensions, namely wav in this case.

--- flactomp3.py
+++ flactomp3.py
@@ -1,69 +1,64 @@
 #
-# FLACtoMP3.py
+# ToMP3.py
 # Author: Jim Sam
 #
-# This script turns high resolution FLAC files into mp3s for mobile devices.
-# It is a work in progress, but it still works.
+# This script turns high resolution files into mp3s.
 #
 # It assumes you have FFMPEG installed on your system in a way that does not
 # require a defined directory to launch.
 #
 
+from linecache import getline
+from subprocess import call
+
 import os
-import pexpect
-import subprocess
 import re
 
-def check_channels(file, hi_res_path):
-    file = str(file)
-    hi_res_path = str(hi_res_path)
+valid_extensions = ['flac','wav']
 
-    data = pexpect.run('ffmpeg -i \"' + hi_res_path + file + '\".flac')
-    data = str(data)
+def check_channels(f, hi_res_path):
+    csv_file = hi_res_path+f+'_tech.csv'
+    command = ['bwfmetaedit','--out-tech='+csv_file,hi_res_path+f+'.wav']
+    call(command)
 
-    if "mono" in data:
-        channels = 1
-    elif "stereo" in data:
-        channels = 2
-
+    line2 = getline(csv_file,2)
+    line2 = line2.split(',')
+    channels = int(line2[4])
+    os.remove(csv_file)
     return channels
 
-def make_command(file, hi_res_path, lo_res_path):
-    file = str(file)
-    hi_res_path = str(hi_res_path)
-    lo_res_path = str(lo_res_path)
-
+def make_command(hi_res_path, lo_res_path, f, extension):
     command = ""
-    channels = check_channels(file, hi_res_path)
+    channels = check_channels(f, hi_res_path)
 
     if channels == 1:
         command = ['ffmpeg','-i',
-                    hi_res_path+file+'.flac',
+                    hi_res_path+f+'.'+extension,
                     '-write_id3v1', '1','-id3v2_version','3',
                     '-dither_method','modified_e_weighted',
                     '-out_sample_rate','48k','-b:a','160k',
-                    lo_res_path+file+'.mp3']
+                    lo_res_path+f+'.mp3']
     if channels == 2:
         command = ['ffmpeg','-i',
-                    hi_res_path+file+'.flac',
+                    hi_res_path+f+'.'+extension,
                     '-write_id3v1','1','-id3v2_version','3',
                     '-dither_method','modified_e_weighted',
                     '-out_sample_rate','48k', '-b:a','320k',
-                    lo_res_path+file+'.mp3']
+                    lo_res_path+f+'.mp3']
     return command
 
 def gimmespace():
-    for i in range(10):
+    for i in range(5):
         print()
 
-def make_file_list(hi_res_path):
+def make_file_list(hi_res_path, extension):
     dir1 = os.listdir(hi_res_path)
     file_list = []
 
-    for file in dir1:
-        file_name=str(file)
-        if file_name[-5:] == ".flac":
-            file_list.append(file_name[:-5])
+    for f in dir1:
+        [file_name, ext] = f.split('.')
+        if ext == extension:
+            file_list.append(file_name)
 
     return file_list
 
@@ -71,25 +66,41 @@ def validate_path(path):
 	result = re.match(r'(^/.+/$)|(^\w:\\$)|(^\w:\\.+\\$)', path)
 	return result
 
-def main():
+def get_extension():
     toggle = False
-    while toggle==False:
+    while not toggle:
+        extension = input("What is the extension of the high-resolution files? (no dot) ")
+        if extension in valid_extensions:
+            toggle=True
+        else:
+            print("\r\nSorry, that's not a valid extension.\r\n")
+    return extension
+
+def get_paths():
+    toggle = False
+    while not toggle:
         hi_res_path = input("The path of the high-resolution files: ")
-        lo_res_path = input("The path of the low-resolution files: ")
+        lo_res_path = input("The path of the low-resolution files:  ")
         a = validate_path(hi_res_path)
         b = validate_path(lo_res_path)
         if a and b:
             toggle=True
         else:
-            print('\nAt least one of those paths was invalid. Please try again.\n')
+            print('\r\nAt least one of those paths was invalid. Please try again.\r\n')
+    return hi_res_path, lo_res_path
 
-    file_list = make_file_list(hi_res_path)
+def main():
+    hi_res_path, lo_res_path = get_paths()
+    extension = get_extension()
+    file_list = make_file_list(hi_res_path, extension)
 
-    for file in file_list:
+    for f in file_list:
         gimmespace()
-        print("Making mp3 for: " + file)
-        command = make_command(file, hi_res_path, lo_res_path)
-        subprocess.call(command)
+        print("Making mp3 for: " + f)
+        command = make_command(hi_res_path, lo_res_path, f.strip(), extension.strip())
+        call(command)
+
+    gimmespace()
 
 if __name__ == '__main__':
     main()
