474bf15876ab4b944a3620ed629a867aefb9d164
Jim Sam <jamessam@users.noreply.github.com>
2017-02-19 17:22:55 -0800

    change command for channel count

--- tomp3_stereo.sh
+++ tomp3_stereo.sh
@@ -7,5 +7,11 @@ cd $1
 for i in ls *.$ext
 do
   f=${i/$ext/mp3}
-  ffmpeg -i "$i" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -out_sample_rate 48k -b:a 320k "$lo_res_path/$f"
+  ffmpeg -i "$i" 2> output.txt
+  if (grep 'mono' output.txt)
+  then
+    ffmpeg -i "$i" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -out_sample_rate 48k -b:a 160k "$lo_res_path/$f"
+  else
+    ffmpeg -i "$i" -write_id3v1 1 -id3v2_version 3 -dither_method modified_e_weighted -out_sample_rate 48k -b:a 320k "$lo_res_path/$f"
+  fi
 done
