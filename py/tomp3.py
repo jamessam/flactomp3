42cf98137d7f148e2c8d580bff1d83266e6cccba
James Sam <jamessam@users.noreply.github.com>
2018-10-31 00:20:43 -0700

    refactored script

--- py/tomp3.py
+++ py/tomp3.py
@@ -1,141 +1,111 @@
-'''tomp3.py turns high resolution files into mp3s.
-
-It assumes you have FFMPEG installed on your system and path.
-'''
-
-from linecache import getline
-from platform import system
-
 import os
+from platform import system
 import re
 import subprocess
 import sys
 
-valid_extensions = ['flac','wav']
 
-def check_channels(hi_res_path, f, extension):
-    real = os.path.join(hi_res_path, f+'.'+extension)
-    new = real.replace('.', '1.')
-    command = ['ffmpeg', '-i', real, new]
+VALID_EXTENSIONS = ['flac','wav']
+
+
+def main():
+    proceed_if_dependencies_there()
+    high_res_path, low_res_path = get_valid_high_and_low_resolution_paths()
+    extension = get_extension()
+    file_list = make_file_list(high_res_path, extension)
+    make_mp3s(high_res_path, low_res_path, file_list, extension)
+
+
+def proceed_if_dependencies_there():
+    if 'ffmpeg version' not in call_subprocess(['ffmpeg','-h']):
+        sys.exit('Please install FFMPEG.')
+
+
+def call_subprocess(command):
     try:
-        output = subprocess.Popen(command, stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT, universal_newlines=True)
-        message = output.stdout.read()
-        output.stdout.close()
-        os.remove(new)
-    except:
-        sys.exit('\r\nHey, something went wrong. Sorry about that.\r\n')
+        output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        message = output.stdout
+    except Exception as error:
+        raise error.with_traceback(sys.exc_info()[2])
+    return message
 
+
+def get_valid_high_and_low_resolution_paths():
+    high_res_path = get_valid_path('high')
+    low_res_path = get_valid_path('low')
+    return high_res_path, low_res_path
+
+
+def get_valid_path(resolution):
+    while True:
+        path = input(f'The path of the {resolution}-resolution files: ')
+        if not validate_path(path):
+            print('That path is invalid. Please try again.')
+            continue
+        break
+    return path
+
+
+def validate_path(path):
+    result = True
+    if not os.path.exists(path):
+        result = False
+    if not special_check_against_bad_windows_path_syntax(path):
+        result = False
+    return result
+
+
+def special_check_against_bad_windows_path_syntax(path):
+    result = True
+    if system() == 'Windows':
+        if not re.match(r'(^\w:\\$)|(^\w:\\.+$)', path):
+            result = False
+    return result
+
+
+def get_extension():
+    while True:
+        extension = input("What is the extension of the high-resolution files? (no dot) ")
+        if extension.casefold() in VALID_EXTENSIONS:
+            break
+        print('\r\nSorry, that\'s not a valid extension.\r\n')
+    return extension
+
+
+def make_file_list(hi_res_path, extension):
+    files_in_dir = os.listdir(hi_res_path)
+    file_list = [f for f in os.listdir(hi_res_path) if f.endswith(extension)]
+    return file_list
+
+
+def make_mp3s(high_res_path, low_res_path, file_list, extension):
+    for f in file_list:
+        print("Making mp3 for: " + f)
+        command = make_command(high_res_path, low_res_path, f.strip(), extension.strip())
+        call_subprocess(command)
+
+
+def make_command(high_res_path, low_res_path, master_file, extension):
+    mp3_file = master_file.replace(extension, 'mp3')
+    full_master = os.path.join(high_res_path, master_file)
+    full_mp3 = os.path.join(low_res_path, mp3_file)
+    command = ['ffmpeg', '-i', full_master, '-write_id3v1', '1','-id3v2_version', '3',
+        '-dither_method','modified_e_weighted', '-out_sample_rate','48k', '-b:a','320k', full_mp3]
+    channels = check_channels(full_master)
+    if channels == 1: 
+        command[12]='160k'
+    return command
+
+
+def check_channels(master_file):
+    command = ['ffmpeg', '-i', master_file]
+    message = call_subprocess(command)
     if 'mono' in message:
         channels = 1
     else:
         channels = 2
-
     return channels
-
-def make_command(hi_res_path, lo_res_path, f, extension):
-    command = ""
-    channels = check_channels(hi_res_path, f, extension)
-
-    if channels == 1:
-        command = ['ffmpeg','-i',
-                    os.path.join(hi_res_path, f + '.' + extension),
-                    '-write_id3v1', '1','-id3v2_version','3',
-                    '-dither_method','modified_e_weighted',
-                    '-out_sample_rate','48k','-b:a','160k',
-                    os.path.join(lo_res_path, f + '.mp3')]
-    if channels == 2:
-        command = ['ffmpeg','-i',
-                    os.path.join(hi_res_path, f + '.' + extension),
-                    '-write_id3v1','1','-id3v2_version','3',
-                    '-dither_method','modified_e_weighted',
-                    '-out_sample_rate','48k', '-b:a','320k',
-                    os.path.join(lo_res_path, f + '.mp3')]
-    return command
-
-def gimmespace():
-    for i in range(5):
-        print()
-
-def make_file_list(hi_res_path, extension):
-    l = len(extension)*-1
-    file_list = [f[:l-1] for f in os.listdir(hi_res_path) if f[l:].lower() == extension.lower()]
-    return file_list
-
-def validate_path(path):
-    if system() == 'Windows':
-        if re.match(r'(^\w:\\$)|(^\w:\\.+\\$)', path):
-            result = True
-
-    if os.path.exists(path):
-        result = True
-    else:
-        result = False
-
-    return result
-
-def get_extension():
-    toggle = False
-    while not toggle:
-        extension = input("What is the extension of the high-resolution files? (no dot) ")
-        if extension in valid_extensions:
-            toggle=True
-        else:
-            print("\r\nSorry, that's not a valid extension.\r\n")
-    return extension
-
-def get_paths():
-    toggle = False
-    while not toggle:
-        hi_res_path = input("The path of the high-resolution files: ")
-        lo_res_path = input("The path of the low-resolution files:  ")
-        a = validate_path(hi_res_path)
-        b = validate_path(lo_res_path)
-        if a and b:
-            toggle=True
-        else:
-            print('\r\nAt least one of those paths was invalid. Please try again.\r\n')
-    return hi_res_path, lo_res_path
-
-def test_apps_there():
-    ffmpeg = False
-
-    # Ensure FFMPEG
-    if system() == 'Darwin':
-        f_command = ['ffmpeg', '-h']
-
-        try:
-            output = subprocess.Popen(f_command, stdout=subprocess.PIPE,
-                    stderr=subprocess.STDOUT, universal_newlines=True)
-            message = output.stdout.read()
-            output.stdout.close()
-            if 'ffmpeg version' in message:
-                ffmpeg = True
-        except FileNotFoundError:
-            pass
-
-    # This script does not ensure non-Mac computers has FFMPEG installed.
-    else:
-        ffmpeg = True
-
-    return ffmpeg
-
-def main():
-    ffmpeg = test_apps_there()
-    if not ffmpeg:
-        sys.exit('Please make sure ffmpeg is installed.\r\n')
-
-    hi_res_path, lo_res_path = get_paths()
-    extension = get_extension()
-    file_list = make_file_list(hi_res_path, extension)
-
-    for f in file_list:
-        gimmespace()
-        print("Making mp3 for: " + f)
-        command = make_command(hi_res_path, lo_res_path, f.strip(), extension.strip())
-        subprocess.call(command)
-
-    gimmespace()
+    
 
 if __name__ == '__main__':
     main()
