c2e68b2189e8485fba693211d7a2283a3f573884
James Sam <jamessam@users.noreply.github.com>
2018-10-31 19:54:46 -0700

    refactor tests, light modifications to prod code

--- py/tests.py
+++ py/tests.py
@@ -1,17 +1,62 @@
 import os
-import subprocess
 from platform import system
+# from subprocess import run
 from unittest import main, TestCase
+from unittest.mock import patch
 
-from tomp3 import check_channels, make_command, make_file_list, test_apps_there, validate_path
+from tomp3 import (
+    call_subprocess, check_channels, get_extension, get_valid_path, 
+    make_command, make_file_list, validate_path
+)
 
 
-class SystemTests(TestCase):
-    def test_system_for_apps(self):
-        f = test_apps_there()
-        self.assertTrue(f)
+def create_sound_file_if_not_there(file):
+    command = ['ffmpeg', '-f', 'lavfi', '-i', 'sine=frequency=1000:duration=5']
+    if not os.path.exists(file):
+        command.append(file)
+        call_subprocess(command)
+
 
 class UnitTestsForFunctions(TestCase):
+    def test_call_subprocess(self):
+        message = str(call_subprocess(['ls']))
+        self.assertIn('tests.py', message)
+        self.assertIn('tomp3.py', message)
+
+    # Note: these next two tests only confirms successful calls. 
+    # Bad calls will trigger an infinite loop.
+    @patch('tomp3.get_input', return_value='/usr/')
+    def test_get_valid_path(self, input):
+        if system() != 'Windows':
+            self.assertEqual('/usr/', get_valid_path('high'))
+
+    @patch('tomp3.get_input', return_value='flac')
+    def test_get_extension(self, input):
+        self.assertEqual(get_extension(), 'flac')
+
+    def test_validate_path(self):
+        if system() == 'Windows':
+            path1 = 'c:\\'
+            path2 = 'c:'
+        elif system() == 'Darwin':
+            path1 = '/users/'
+            path2 = '/users/fake/path'
+        else:
+            path1 = '/usr/'
+            path2 = '/users/fake/path'
+        self.assertTrue(validate_path(path1))
+        self.assertFalse(validate_path(path2))
+    
+    def test_make_file_list(self):
+        file_list = make_file_list(os.getcwd(), 'py')
+        self.assertEqual(['tests.py', 'tomp3.py'], file_list)
+        file_list = make_file_list(os.getcwd(), 'mp3')
+        self.assertEqual([], file_list)
+        file_list = make_file_list(os.getcwd(), 'md')
+        self.assertEqual([], file_list)
+
+
+class UnitTestsForFunctionsWhereSoundFileIsRequired(TestCase):
     folder_exists = False
     testing_path = 'testing'
 
@@ -28,31 +73,15 @@ class UnitTestsForFunctions(TestCase):
         else:
             os.mkdir(self.testing_path)
 
-        command = ['ffmpeg', '-f', 'lavfi', '-i' ,'sine=frequency=1000:duration=5']
         if not os.path.exists(self.throwaway):
             throw = open(self.throwaway, 'w')
             throw.write('Test file. Please delete this file if you find it.')
             throw.close()
-        if not os.path.exists(self.mp3c):
-            command.append(self.mp3c)
-            subprocess.call(command)
-            del command[5]
-        if not os.path.exists(self.mp3d):
-            command.append(self.mp3d)
-            subprocess.call(command)
-            del command[5]
-        if not os.path.exists(self.wav1):
-            command.append(self.wav1)
-            subprocess.call(command)
-            del command[5]
-        if not os.path.exists(self.wav2):
-            command.append(self.wav2)
-            subprocess.call(command)
-            del command[5]
-        if not os.path.exists(self.flac):
-            command.append(self.flac)
-            subprocess.call(command)
-            del command[5]
+        create_sound_file_if_not_there(self.mp3c)
+        create_sound_file_if_not_there(self.mp3d)
+        create_sound_file_if_not_there(self.wav1)
+        create_sound_file_if_not_there(self.wav2)
+        create_sound_file_if_not_there(self.flac)
 
     def tearDown(self):
         os.remove(self.throwaway)
@@ -61,50 +90,24 @@ class UnitTestsForFunctions(TestCase):
         os.remove(self.wav1)
         os.remove(self.wav2)
         os.remove(self.flac)
-
         if not self.folder_exists:
             os.rmdir(self.testing_path)
 
     def test_check_channels(self):
-        flac_channels = check_channels(self.testing_path, 'yo', 'flac')
-        wav_channels = check_channels(self.testing_path, '1', 'wav')
+        test1 = os.path.join(self.testing_path, 'yo.flac')
+        test2 = os.path.join(self.testing_path, '1.wav')
+        flac_channels = check_channels(test1)
+        wav_channels = check_channels(test2)
         self.assertEqual(flac_channels, 1)
         self.assertEqual(wav_channels, 1)
 
     def test_make_command(self):
-        hi_path = 'testing'
-        lo_path = 'testing'
-        f       = '1'
-        ext     = 'wav'
-        expected = ['ffmpeg','-i',
-                    'testing/1.wav',
+        expected = ['ffmpeg','-i', 'testing/1.wav',
                     '-write_id3v1', '1','-id3v2_version','3',
                     '-dither_method','modified_e_weighted',
-                    '-out_sample_rate','48k','-b:a','160k',
-                    'testing/1.mp3']
-        tested = make_command(hi_path, lo_path, f, ext)
-        self.assertEqual(tested, expected)
+                    '-out_sample_rate','48k','-b:a','160k', 'testing/1.mp3']
+        tested = make_command('testing', 'testing', '1.wav', 'wav')
+        self.assertEqual(expected, tested)
 
-    def test_path_validation(self):
-        if system() == "Windows":
-            path1 = 'c:\\'
-            path2 = 'c:'
-        elif system() == "Darwin":
-            path1 = '/users/'
-            path2 = '/users/fake/path'
-        else:
-            pass
-
-        self.assertTrue(validate_path(path1))
-        self.assertFalse(validate_path(path2))
-
-    def test_file_list(self):
-        file_list = make_file_list(self.testing_path, 'wav')
-        should = ['1', '2']
-        self.assertEqual(file_list, should)
-
-        file_list = make_file_list(self.testing_path, 'flac')
-        should = ['yo']
-        self.assertEqual(file_list, should)
 
 main()
--- py/tomp3.py
+++ py/tomp3.py
@@ -23,7 +23,8 @@ def proceed_if_dependencies_there():
 
 def call_subprocess(command):
     try:
-        output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        output = subprocess.run(command, stdout=subprocess.PIPE, 
+            stderr=subprocess.STDOUT, universal_newlines=True)
         message = output.stdout
     except Exception as error:
         raise error.with_traceback(sys.exc_info()[2])
@@ -38,12 +39,15 @@ def get_valid_high_and_low_resolution_paths():
 
 def get_valid_path(resolution):
     while True:
-        path = input(f'The path of the {resolution}-resolution files: ')
-        if not validate_path(path):
-            print('That path is invalid. Please try again.')
-            continue
-        break
+        path = get_input(f'The path of the {resolution}-resolution files: ')
+        if validate_path(path):
+            break
+        print('That path is invalid. Please try again.')        
     return path
+
+
+def get_input(text):
+    return input(text)
 
 
 def validate_path(path):
@@ -65,7 +69,7 @@ def special_check_against_bad_windows_path_syntax(path):
 
 def get_extension():
     while True:
-        extension = input("What is the extension of the high-resolution files? (no dot) ")
+        extension = get_input("What is the extension of the high-resolution files? (no dot) ")
         if extension.casefold() in VALID_EXTENSIONS:
             break
         print('\r\nSorry, that\'s not a valid extension.\r\n')
@@ -75,13 +79,15 @@ def get_extension():
 def make_file_list(hi_res_path, extension):
     files_in_dir = os.listdir(hi_res_path)
     file_list = [f for f in os.listdir(hi_res_path) if f.endswith(extension)]
+    file_list.sort()
     return file_list
 
 
 def make_mp3s(high_res_path, low_res_path, file_list, extension):
     for f in file_list:
         print("Making mp3 for: " + f)
-        command = make_command(high_res_path, low_res_path, f.strip(), extension.strip())
+        command = make_command(high_res_path, low_res_path, f.strip(), 
+            extension.strip())
         call_subprocess(command)
 
 
@@ -89,8 +95,10 @@ def make_command(high_res_path, low_res_path, master_file, extension):
     mp3_file = master_file.replace(extension, 'mp3')
     full_master = os.path.join(high_res_path, master_file)
     full_mp3 = os.path.join(low_res_path, mp3_file)
-    command = ['ffmpeg', '-i', full_master, '-write_id3v1', '1','-id3v2_version', '3',
-        '-dither_method','modified_e_weighted', '-out_sample_rate','48k', '-b:a','320k', full_mp3]
+    command = ['ffmpeg', '-i', full_master, 
+        '-write_id3v1', '1','-id3v2_version', '3',
+        '-dither_method','modified_e_weighted', 
+        '-out_sample_rate','48k', '-b:a','320k', full_mp3]
     channels = check_channels(full_master)
     if channels == 1: 
         command[12]='160k'
